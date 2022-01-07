1637ad221a4980375f94f97afe243b4f8d159a62
Hector Hernandez <hhernandez@ryze-tech.com>
2022-01-07 03:07:28 -0500

    New update

--- FusionServer/Src/scripts/updateFusion.sh
+++ FusionServer/Src/scripts/updateFusion.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo git pull
-cd /usr/Fusion/FusionServer/Build
+git clone https://github.com/hhernandez92/Nano.git /home/pi/Desktop/Nano
+cd /home/pi/Desktop/Nano/FusionServer/Src
 sudo npm i --production --unsafe-perm
+cd ..
+mv Src Build
+cd ..
+cd ..
+sudo rm -R /usr/Fusion
+sudo mv /home/pi/Desktop/Nano /usr/Fusion
+sudo forever restart
