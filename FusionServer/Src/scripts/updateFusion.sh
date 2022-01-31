520e0d1de675cc6c21347a3e6745dea0bb8cb8f0
Hector Hernandez <hhernandez@ryze-tech.com>
2022-01-31 15:29:01 -0500

    Modify update script

--- FusionServer/Src/scripts/updateFusion.sh
+++ FusionServer/Src/scripts/updateFusion.sh
@@ -3,7 +3,7 @@ sudo git clone https://github.com/hhernandez92/Nano.git /home/pi/Desktop/Nano
 cd /home/pi/Desktop/Nano/FusionServer/Src
 sudo npm i --production --unsafe-perm
 cd ..
-mv Src Build
+sudo mv Src Build
 sudo rm -R /usr/Fusion
 sudo mv /home/pi/Desktop/Nano /usr/Fusion
 sudo forever restartall
